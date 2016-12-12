08f49998a2ec1c94759c4241a355103e5ed29a19
Roger <rogerkueng@gmail.com>
2016-12-12 08:51:53 +0100

    Make the main more explicit concerning missing implementation

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,14 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        RobotArm robotArm = new RobotArm(10, 10);
+        try {
+            RobotArm robotArm = new RobotArm(10, 10);
+        } catch (Exception e) {
+            System.out.println("You probably should start by implementing the constructor :)");
+
+        }
         System.out.println("Congratulations, you run the application");
+
     }
 
 }
